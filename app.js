e9f502c5607a507d5655a66c289ea0f37da8899f
Grace Ho <rainczech@gmail.com>
2018-03-11 21:34:23 -0500

    button values and api onto DOM

--- app.js
+++ app.js
@@ -6,28 +6,58 @@ var topic = ["cat", "dog", "horse", "pig"];
 
 
 
-
 // functions
-function start(){
+function createButton(){
 for (var i=0; i<topic.length; i++) {
-    var newButton = $("<button></button>");
+    var newButton = $("<button id='animals'>");
+    newButton.val(topic[i]);
     newButton.text(topic[i]);
     $("#buttons").append(newButton);
 }};
 
-$("button").on("click", function animal(){
-    var animal.val(this);
-    var queryURL = "https://api.giphy.com/v1/gifts/search?q="+animal+"&api-key=uvMzqP4htDb03CqytfpkBbkuGbBVLLj9$limit=10";
+$("#buttons").on("click","#animals", function animal(){
+    $("#image-place").empty();
+    var animal = $(this).val();
+    console.log(animal);
+    var queryURL =" http://api.giphy.com/v1/gifs/search?q="+animal +"&api_key=uvMzqP4htDb03CqytfpkBbkuGbBVLLj9&limit=3"
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function(response){
-        var imageUrl = response.data.image_original_url;
-        var image = $("<img>");
-        image.attr("src", imageUrl);
-        $("#image-place").append(image);
+        console.log(response);
+        var results = response.data;
+        for (var i=0; i<results.length; i++){
+            var gifDiv = $("<div class='complete'>");
+            var rating = results[i].rating;
+            var ratingP=$("<p>").text("Rating: "+rating);
+            var topicImage = $("<img>");
+            topicImage.attr("src", results[i].images.fixed_height_still.url);
+            topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+            topicImage.attr("data-animate", results[i].images.fixed_height.url);
+            topicImage.attr("data-state", "still");
+            topicImage.attr("class", "gif");
+
+            gifDiv.prepend(ratingP);
+            gifDiv.prepend(topicImage);
+
+            $("#image-place").prepend(gifDiv);
+        }
+})
+
+function submit(){
+    ("#submitButton").on("click", function(){
+        event.preventDefault();
+        var userInputVal = $("#user-input").val();
+        console.log(userInputVal);
+        topic.push(userInputVal);
+        createButton();
+
+
+
+
     })
+}
 })
 
 // psudocode
@@ -46,6 +76,4 @@ $("button").on("click", function animal(){
 
 
 // main
-start();
-
-
+    createButton()
